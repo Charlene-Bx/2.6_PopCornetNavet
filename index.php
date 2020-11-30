a08d2a9de99b441326117726fc0dfefe4c20ac36
Charlene-Bx <charlene.bonnardeaux@gmail.com>
2020-11-30 14:06:31 +0100

    first html > landing page

--- index.php
+++ index.php
@@ -9,7 +9,27 @@
     <link rel="stylesheet" href="./asset/style/style.css">
 </head>
 <body>
-    
+    <article>
+        <form action="./asset/authentification.php" method="POST">
+            <div>
+                <label for="eMail">Adresse e-mail:</label>
+                <input type="text" name="eMail" id="eMail">
+            </div>
+            <div>
+                <label for="MdP">Mot de passe:</label>
+                <input type="text" name="MdP" id="MdP">
+            </div>
+            <div>
+                <button type="submit">Me connecter</button>
+            </div>
+        </form>
+        <div>
+            <ul>
+                <li><a href="newAccount.php">Créer un nouveau compte</a></li>
+                <li><a href="newPassword.php">Mot de passe oublié?</a></li>
+            </ul>
+        </div>
+    </article>
 <script src="./asset/js/index.js"></script>
 </body>
 </html>
