436bdf8c296f3396dfb8d6bc92f334ed4335db0c
Nick-C0DE <chesnelnicolas800@gmail.com>
2020-12-07 19:39:31 +0100

    carousel

--- home.php
+++ home.php
@@ -1,24 +1,29 @@
+<?php
+    require './asset/navbar.php';
+    
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-   
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@1,700&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
-<body>
+<body style= "font-family: 'Ubuntu', sans-serif;">
 
 <div class="card mb-3 mt-md-4 pt-md-4" style="max-width: 100%; height:600px;">
 <div class= "container-fluid">
-<div class="position-relative border display-4" style="left:90px; top:30px; width:40%; z-index:2;">
+<div class="position-relative border display-4" style="left:90px; top:20px; width:40%; z-index:2;">
  LE MEILLEUR DU PIRE
 </div></div>
   <div class="row no-gutters">
     <div class="col-md-8">
     <iframe class="card-img" width="600px" height="550px" src="https://www.youtube.com/embed/KylLpVUT00M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-    <div class="col-md-4 bg-primary text-white">
+    <div class="col-md-4">
     
       <div class="card-body text-center-lg ">
         <h5 class="card-title">ACTION</h5> 
@@ -27,7 +32,7 @@
         <p class="card-text">Lorem ipsum dolor sit amet, consecteturadipiscing elit. Pellentesque nunc mi,mollis vitae lectus tristique, dignissieleifend tortor. Suspendisse et ligularcu. Duis dapibus, lorem sed ultriciesegestas, tellus nulla ornare purus, nonconsectetur nibh sapien tempor velit!</p>
         </div>
         <div class="card-content mt-md-5 pt-md-5 text-center">
-        <button type="button" class="btn btn-default btn-light btn-lg">voir le film</button>
+        <button type="button" class="btn btn-default btn-light btn-lg rounded-pill">voir le film</button>
         <br/>
         <a href="#" class="stretched-link">Lire les critiques</a>
         </div>
@@ -37,3 +42,139 @@
   </div>
 </body>
 </html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+  /* Make the image fully responsive */
+  .carousel-inner iframe {
+    width: 400px;
+    height: 400px;
+  }
+  </style>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<div id="demo1" class="carousel slide ml-md-5 pl-md-5" data-ride="carousel">
+
+<!-- Indicators -->
+<ul class="carousel-indicators">
+  <li data-target="#demo1" data-slide-to="0" class="active"></li>
+  <li data-target="#demo1" data-slide-to="1"></li>
+  
+</ul>
+
+<!-- The slideshow -->
+<div class="carousel-inner">
+  <div class="carousel-item active">
+   <iframe class="card-img" src="https://www.youtube.com/embed/KylLpVUT00M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe class="card-img" src="https://www.youtube.com/embed/KylLpVUT00M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe class="card-img" src="https://www.youtube.com/embed/KylLpVUT00M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="card-img" src="https://www.youtube.com/embed/KylLpVUT00M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+  </div>
+  <div class="carousel-item">
+  <iframe class="card-img" src="https://www.youtube.com/embed/_stqtY9egkg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe class="card-img" src="https://www.youtube.com/embed/_stqtY9egkg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe class="card-img" src="https://www.youtube.com/embed/_stqtY9egkg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="card-img" src="https://www.youtube.com/embed/_stqtY9egkg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+  
+</div>
+
+<!-- Left and right controls -->
+<a class="carousel-control-prev" href="#demo1" data-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</a>
+<a class="carousel-control-next" href="#demo1" data-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</a>
+</div>
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+  /* Make the image fully responsive */
+  .carousel-inner iframe {
+    width: 400px;
+    height: 400px;
+  }
+  </style>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<div id="demo" class="carousel slide ml-md-5 pl-md-5" data-ride="carousel">
+
+<!-- Indicators -->
+<ul class="carousel-indicators">
+  <li data-target="#demo" data-slide-to="0" class="active"></li>
+  <li data-target="#demo" data-slide-to="1"></li>
+  
+</ul>
+
+<!-- The slideshow -->
+<div class="carousel-inner">
+  <div class="carousel-item active">
+   <iframe class="card-img" src="https://www.youtube.com/embed/KylLpVUT00M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe class="card-img" src="https://www.youtube.com/embed/KylLpVUT00M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe class="card-img" src="https://www.youtube.com/embed/KylLpVUT00M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="card-img" src="https://www.youtube.com/embed/KylLpVUT00M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+  </div>
+  <div class="carousel-item">
+  <iframe class="card-img" src="https://www.youtube.com/embed/_stqtY9egkg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe class="card-img" src="https://www.youtube.com/embed/_stqtY9egkg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe class="card-img" src="https://www.youtube.com/embed/_stqtY9egkg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="card-img" src="https://www.youtube.com/embed/_stqtY9egkg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+  
+</div>
+
+<!-- Left and right controls -->
+<a class="carousel-control-prev" href="#demo" data-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</a>
+<a class="carousel-control-next" href="#demo" data-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</a>
+</div>
+</body>
+</html>
+
+
+
