8b4cca9513177403774c1ce986bf0f97f5aa7ca2
Nick-C0DE <chesnelnicolas800@gmail.com>
2020-12-07 16:49:17 +0100

    home.php

---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+   
+    <title>Document</title>
+</head>
+<body>
+
+<div class="card mb-3 mt-md-4 pt-md-4" style="max-width: 100%; height:600px;">
+<div class= "container-fluid">
+<div class="position-relative border display-4" style="left:90px; top:30px; width:40%; z-index:2;">
+ LE MEILLEUR DU PIRE
+</div></div>
+  <div class="row no-gutters">
+    <div class="col-md-8">
+    <iframe class="card-img" width="600px" height="550px" src="https://www.youtube.com/embed/KylLpVUT00M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div class="col-md-4 bg-primary text-white">
+    
+      <div class="card-body text-center-lg ">
+        <h5 class="card-title">ACTION</h5> 
+        <div class="card-body text-center-lg ml-md-4">
+        <h1 class="card-title">Titre du film nul</h1>
+        <p class="card-text">Lorem ipsum dolor sit amet, consecteturadipiscing elit. Pellentesque nunc mi,mollis vitae lectus tristique, dignissieleifend tortor. Suspendisse et ligularcu. Duis dapibus, lorem sed ultriciesegestas, tellus nulla ornare purus, nonconsectetur nibh sapien tempor velit!</p>
+        </div>
+        <div class="card-content mt-md-5 pt-md-5 text-center">
+        <button type="button" class="btn btn-default btn-light btn-lg">voir le film</button>
+        <br/>
+        <a href="#" class="stretched-link">Lire les critiques</a>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</body>
+</html>
