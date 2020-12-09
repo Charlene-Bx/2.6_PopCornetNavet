a352b8bc90a9e04a1c30ace17c0e7ded959116dc
Nick-C0DE <chesnelnicolas800@gmail.com>
2020-12-09 16:39:53 +0100

    nico

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 
 <html lang="en">
 <head>
@@ -10,13 +10,54 @@
     <link rel="stylesheet" href="./asset/style/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">    
+        </head>
+        <body>
+        <h1>hellotest</h1>  
+            <?php
+            // require './asset/navbar.php';
+            // require 'login.php';
+            // require './action.php';
+            ?>
+        <script src="./asset/js/index.js"></script>
+        
+        </body>
+        </html> -->
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Registration</title>
 </head>
 <body>
-    <?php
-    require './asset/navbar.php';
-    require 'login.php';
-    require './action.php';
-    ?>
-<script src="./asset/js/index.js"></script>
+
+
+   <div class="logo">
+       <img src="images/logo.png" alt="">
+   </div>
+
+    <div class="wrapper">
+    
+        <div class="title">login</div>
+
+        <form class="form" method="POST" action="">
+            
+            <div class="input_fild">
+                <label for="mail">email address</label>
+                <input name="mail" type="email" class="input" id="mail">
+            </div>
+            <div class="input_fild">
+                <label for="">password</label>
+                <input name="pass" type="password" class="input" id="pass">
+            </div>
+            <input type="submit" name="login" value="Login">
+            
+            <a href="signup.php">create</a>
+            <?php include 'brain/login.php';?>
+        </form>
+    </div>
+        
+
 </body>
 </html>
---
+++ signup.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/signup.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="logo">
+       <img src="images/logo.png" alt="">
+   </div>
+
+<div class="wrapper">
+        <div class="title">
+            SIGN UP
+        </div>
+
+        <form class="form" method="POST" action="">
+            <div class="input_fild">
+                <label for="fname">first name</label>
+                <input name="fname" type="text" class="input" id="fname" required>
+            </div>
+            <div class="input_fild">
+                <label for="lname">last name</label>
+                <input name="lname" type="text" class="input" id="lname" required>
+            </div>
+            <div class="input_fild">
+                <label for="email1">email address</label>
+                <input name="email1" type="email1" class="input" id="email1" required>
+            </div>
+            <div class="input_fild">
+                <label for="pass1">password</label>
+                <input name="pass1" type="password" class="input" id="pass1" required>
+            </div>
+            <div class="input_fild">
+                <label for="pass2">confirm password</label>
+                <input name="pass2" type="password" class="input" id="pass2" required>
+            </div>
+
+            <input type="submit" name="submit" value="Subscribe" style="width:auto">
+
+            <?php include 'brain/signin.php'; ?>
+        </form>
+    </div>
+    </body>
+</html>
