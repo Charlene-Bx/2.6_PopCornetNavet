b79747bd4b04d0b1e41aec8fe3a8d3808614c5b8
Nick-C0DE <chesnelnicolas800@gmail.com>
2020-12-10 07:48:30 +0100

    nico

--- index.php
+++ index.php
@@ -9,24 +9,27 @@
     <link rel="stylesheet" href="./view/style/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous"> 
     <link rel="stylesheet" href="./view/style/style.css">
       
 </head>
 <body>
+
     <?php
-    require_once './view/navbar.php';
-    // require_once './view/login.php';
+    require_once './view/login.php';
     require_once './brain/login.php';
+
+    // require_once './view/signin.php';
+    // require_once './brain/createAccount.php';
+    //
+    // >> A FAIRE:
+    //  Animation Js pour passer de login à sign lorsque l'utilisateur clique sur le lien "Tu n'as pas encore de compte?"
+    //  Style de l'input submit (Est-ce que ça fonctionnerait aussi avec un bouton?)
+    //  Requête db pour la login page; quand l'utilisateur clique sur "me connecter" si il est dans les bases de donnée, l'envoyer sur homePage.php
+
     ?>
-    <div id="api" class="posters">
-                <h1>film connue</h1>
-    </div>
-
-    <div id="api2" class="posters1">
-                <h1>action</h1>
-    </div>
-
     
 <script src="./view/js/index.js"></script>
 </body>
